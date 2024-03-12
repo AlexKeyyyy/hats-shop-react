7aa2a6af67dbca73cff5e07b2a64c322f21e3389
Alex Koba_Desktop <koba.ayu@edu.spbstu.ru>
2024-03-12 05:52:54 +0300

    fix images size!!!!

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -11,15 +11,15 @@ const Item = ({product, className, onAdd}) => {
         <div className={'product' + className}>
             <div className={'img'}>
                 <img src={product.image} alt={product.title}/>
-                <div className={'title'}>{product.title}</div>
-                <div className={'description'}>{product.description}</div>
-                <div className={'price'}>
-                    <span>Стоимость: <b>{product.price}</b></span>
-                </div>
-                <button className={'add-btn'} onClick={onAddHandler}>
-                    Добавить в корзину
-                </button>
             </div>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
+            <div className={'price'}>
+                <span>Стоимость: <b>{product.price}</b></span>
+            </div>
+            <button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </button>
 
         </div>
     )
